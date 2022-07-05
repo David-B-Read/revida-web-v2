a8045b20fcde79e08bb880bf91243fdde2d1abef
David-B-Read <david.read@digital.education.gov.uk>
2022-07-05 17:23:44 +0100

    Create logo component

---
+++ revida/src/Logo/component.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Logo() {
+  // Import result is the URL of your image
+  return <img src={"images/revida-logo-large.png"} alt="Revida - Development, Consulting, Coaching" />;
+}
+
+export default Logo;
